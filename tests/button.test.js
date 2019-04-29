9075291ff100811bca6bb59742645536542b8e67
likaiqiang <921085426@qq.com>
2019-04-29 08:04:14 +0800

    找个按钮测一下

--- tests/button.test.js
+++ tests/button.test.js
@@ -1,31 +1,12 @@
 import React, { Component } from 'react';
 import { render, mount } from 'enzyme';
-import { Button } from 'antd';
+import UiButton from '../src/components/index'
 
 describe('Button', () => {
   it('按钮测试', () => {
     const wrapper = render(
-      <Button>测试</Button>
+      <UiButton>测试</UiButton>
     );
     expect(wrapper).toMatchSnapshot();
-  });
-
-  it('测试加载中', () => {
-    class DefaultButton extends Component {
-      state = {
-        loading: false,
-      };
-      enterLoading = () => {
-        this.setState({ loading: true });
-      }
-      render() {
-        return <Button loading={this.state.loading} onClick={this.enterLoading}>Button</Button>;
-      }
-    }
-    const wrapper = mount(
-      <DefaultButton />
-    );
-    wrapper.simulate('click');
-    expect(wrapper.find('.ant-btn-loading').length).toBe(1);
-  });
+  })
 })
